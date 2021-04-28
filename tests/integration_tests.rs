00c7199f00f1dfd0b0c9c832bf268f07444f603c
Yves Bonami <yves.bonami@reniver.eu>
2021-04-28 08:29:17 +0200

    Update test

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -1,28 +1,7 @@
-
-use std::time::Duration;
-
-use marvel_api_rs::{api::Fault, model::Character};
-
-
+use marvel_api_rs::model::Character;
 
 #[test]
 fn get_all_characters_returns_200() {
-    // let mut res =surf::get("https://gateway.marvel.com/v1/public/characters?apikey=aa03547a3f992f9008cb460f61ce8b31&ts=1619535520001&hash=73a10fdf995f452487852e513efba4a9")
-    // .await();
-
     let response = Character::get_all(None).unwrap();
-
-    println!("{:?}", response);
-    // println!("{:?}", res.text().unwrap());
-    // match Character::get_all(None) {
-    //     Ok(w) => {
-    //         assert_eq!(w.code.unwrap(), 200);
-    //     }
-    //     Err(error) => {
-    //         println!("{:?}", error);
-    //         assert!(false);
-    //     }
-    // }
-
-    assert!(false)
+    assert_eq!(200, response.code.unwrap());
 }
